8cb9f6bfc17999f84a21137c800d36748c940ac1
diogozonta <diogo_alveszonta2015@hotmail.com>
2022-09-06 15:19:19 -0300

    Alteração das propriedas da form "Principal.cs".

--- MultApps/clients/MultApps.Client.Windows/Program.cs
+++ MultApps/clients/MultApps.Client.Windows/Program.cs
@@ -11,7 +11,6 @@ namespace MultApps.Client.Windows
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
         }
     }
 }
